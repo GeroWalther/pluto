71a7a5df19b2da42347af571d9ca0b84bd65c2ca
GeroWalther <gero.walther@gmail.com>
2024-03-25 19:55:59 +0100

    upload thing

--- src/components/test/TestUploadThing.tsx
+++ src/components/test/TestUploadThing.tsx
@@ -1,31 +1,23 @@
-"use client";
-import { UploadButton } from "@/lib/uploadthing";
-import { cn } from "@/lib/utils";
-import "@uploadthing/react/styles.css";
-import Image from "next/image";
-import { useState } from "react";
-import { buttonVariants } from "../ui/button";
+'use client';
+import { UploadDropzone } from '@/lib/uploadthing';
+import { useState } from 'react';
+
 export default function Home() {
-  const [url, setUrl] = useState<string>("");
+  const [url, setUrl] = useState<string>('');
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton
-        endpoint="imageUploader"
+    <div className='flex min-h-screen flex-col items-center justify-center p-2'>
+      <UploadDropzone
+        endpoint='imageUploader'
         onClientUploadComplete={(res) => {
           // Do something with the response
-          setUrl(res[0].url);
+          setUrl(res?.[0]?.url);
         }}
         onUploadError={(error: Error) => {
           // Do something with the error.
           alert(`ERROR! ${error.message}`);
         }}
-        className={buttonVariants({
-          variant: "default",
-          size: "lg",
-        })}
       />
-
-      <img src={url} width={200} height={200} alt="test" />
+      {url && <img src={url} width={200} height={200} alt='uploaded image' />}
     </div>
   );
 }
