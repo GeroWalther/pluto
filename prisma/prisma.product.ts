973b0089adfb0aa947be3973bb3bdb6310e1f797
GeroWalther <gero.walther@gmail.com>
2024-03-25 22:41:58 +0100

    upload thing

---
+++ prisma/prisma.product.ts
@@ -0,0 +1,60 @@
+import prisma from '../src/db/db';
+
+// READ
+export const getAllProducts = async () => {
+  const products = await prisma.product.findMany({
+    include: { user: true }, // Include user data in the response
+  });
+  return products;
+};
+
+export const getProductById = async (id: string) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+    include: { user: true }, // Include user data in the response
+  });
+  return product;
+};
+
+// CREATE
+export interface CreateProductType {
+  image: string;
+  name: string;
+  url: string;
+  description: string;
+  price: number;
+  user: {
+    connect: {
+      id: string;
+    };
+  };
+}
+
+export const createProduct = async (productData: CreateProductType) => {
+  const product = await prisma.product.create({
+    data: productData,
+  });
+  return product;
+};
+
+// UPDATE
+export type UpdateProductType = Partial<CreateProductType>; // Allows updating any combination of product properties
+
+export const updateProduct = async (
+  id: string,
+  updateData: UpdateProductType
+) => {
+  const product = await prisma.product.update({
+    where: { id },
+    data: updateData,
+  });
+  return product;
+};
+
+// DELETE
+export const deleteProduct = async (id: string) => {
+  const product = await prisma.product.delete({
+    where: { id },
+  });
+  return product;
+};
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,39 +1,37 @@
-import prisma from "@/db/db";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { UploadThingError } from 'uploadthing/server';
+import { createProduct } from '../../../../prisma/prisma.product';
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: '4MB' } })
     .middleware(async ({ req }) => {
       const session = await getServerSession(authOptions);
-      if (!session) throw new UploadThingError("Unauthorized");
+      if (!session) throw new UploadThingError('Unauthorized');
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: session.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      const uploadProduct = await prisma.product.create({
-        data: {
-          image: file.name,
-          name: "name",
-          url: file.url,
-          description: "description",
-          price: 1000,
-          user: {
-            connect: {
-              id: metadata.userId,
-            },
+      const uploadProduct = await createProduct({
+        image: file.name,
+        name: 'name',
+        url: file.url,
+        description: 'description',
+        price: 1000,
+        user: {
+          connect: {
+            id: metadata.userId,
           },
         },
       });
 
       if (!uploadProduct) {
-        console.error("Failed to upload product");
-        throw new UploadThingError("Failed to upload product");
+        console.error('Failed to upload product');
+        throw new UploadThingError('Failed to upload product');
       }
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
