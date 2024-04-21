e4045986f6f9fec8b08c303a8e75aba6d0055df0
GeroWalther <gero.walther@gmail.com>
2024-04-21 17:42:49 +0200

    added productUpdate trpc

--- src/trpc/api/admin.ts
+++ src/trpc/api/admin.ts
@@ -1,8 +1,31 @@
+import { z } from 'zod';
 import { adminProcedure, router } from '../trpc';
-import { getPendingProductsController } from './controller/admin';
+import {
+  getPendingProductsController,
+  updatePendingProductController,
+} from './controller/admin';
 
 export const adminRouter = router({
   getPendingProducts: adminProcedure.query(async () => {
     return await getPendingProductsController();
   }),
+  updatePendingProducts: adminProcedure
+    .input(z.object({ id: z.string(), updateString: z.string() }))
+    .mutation(async ({ input }) => {
+      const updatedProduct = await updatePendingProductController(
+        input.id,
+        input.updateString
+      );
+      return updatedProduct;
+    }),
+
+  // getAllProdsOnSale:  adminProcedure.query(async () => {
+  //   return await getAllProdsController();
+  // },
+
+  // getSalesdata: adminProcedure.query(async () => {
+  //   return await getSalesDataController()
+  // }),
+
+  // getNumOfUsers
 });
--- src/trpc/api/controller/admin.ts
+++ src/trpc/api/controller/admin.ts
@@ -1,5 +1,6 @@
-import { getAllPendingProducts } from '@/db/prisma.product';
+import { getAllPendingProducts, updateProduct } from '@/db/prisma.product';
 import { TRPCError } from '@trpc/server';
+import prisma from '@/db/db';
 
 export async function getPendingProductsController() {
   const pendingProds = await getAllPendingProducts();
@@ -16,4 +17,36 @@ export async function getPendingProductsController() {
   }
 
   return pendingProds;
+}
+
+export async function updatePendingProductController(
+  id: string,
+  updateString: string
+) {
+  if (!id || updateString !== 'APPROVED' || 'REJECTED') {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: 'Invalid input',
+    });
+  }
+
+  // Update product in DB
+  const updatedProduct = await prisma.product.update({
+    where: { id },
+    data: {
+      status: updateString,
+    },
+  });
+
+  if (!updatedProduct) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Problem updating product',
+    });
+  }
+
+  return {
+    message: `Product ${updateString.toLowerCase()} successfully!`,
+    updatedProduct,
+  };
 }
