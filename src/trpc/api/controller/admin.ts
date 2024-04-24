625968a4a0071bcd1a0772c8aae1e1f48aac7a83
GeroWalther <gero.walther@gmail.com>
2024-04-24 17:07:09 +0200

    working update trpcc

--- src/trpc/api/controller/admin.ts
+++ src/trpc/api/controller/admin.ts
@@ -1,21 +1,21 @@
-import prisma from "@/db/db";
-import { getAllPendingProducts, updateProduct } from "@/db/prisma.product";
-import { TRPCError } from "@trpc/server";
+import prisma from '@/db/db';
+import { getAllPendingProducts, updateProduct } from '@/db/prisma.product';
+import { TRPCError } from '@trpc/server';
 
 export async function getPendingProductsController() {
   const pendingProds = await getAllPendingProducts();
 
   if (!pendingProds) {
     throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Problem getting pending products",
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Problem getting pending products',
     });
   }
 
   if (pendingProds.length === 0) {
     throw new TRPCError({
-      code: "NOT_FOUND",
-      message: "No pending products found",
+      code: 'NOT_FOUND',
+      message: 'No pending products found',
     });
   }
 
@@ -26,10 +26,16 @@ export async function updatePendingProductController(
   id: string,
   updateString: string
 ) {
-  if (!id || updateString !== "APPROVED" || "REJECTED") {
+  if (!id) {
     throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Invalid input",
+      code: 'BAD_REQUEST',
+      message: 'Invalid input id',
+    });
+  }
+  if (updateString !== 'APPROVED' && updateString !== 'REJECTED') {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: 'Invalid input',
     });
   }
 
@@ -43,8 +49,8 @@ export async function updatePendingProductController(
 
   if (!updatedProduct) {
     throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Problem updating product",
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Problem updating product',
     });
   }
 
