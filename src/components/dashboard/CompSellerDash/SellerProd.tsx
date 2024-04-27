5260775af7ebc66ece2ea8c305b059e275e63528
Ahanaf Islam <ahanafislamworks@gmail.com>
2024-04-27 17:43:50 +0600

    bug fixes

--- src/components/dashboard/CompSellerDash/SellerProd.tsx
+++ src/components/dashboard/CompSellerDash/SellerProd.tsx
@@ -1,27 +1,45 @@
-import Loader from '@/components/Loader/Loader';
-import MasterTable from '@/components/Table/MasterTable';
-import UploadDrawerDialog from '@/components/comp/UploadDrawerDialog';
-import { trpc } from '@/trpc/client';
-import React from 'react';
+"use client";
+import Loader from "@/components/Loader/Loader";
+import MasterTable from "@/components/Table/MasterTable";
+import UploadDrawerDialog from "@/components/comp/UploadDrawerDialog";
+import { trpc } from "@/trpc/client";
+import React from "react";
 
 export default function SellerProd() {
-  const { data: products, isLoading } = trpc.seller.getAllProducts.useQuery();
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = trpc.seller.getAllProducts.useQuery();
+
+  if (error) {
+    return (
+      <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h3 className="text-2xl font-bold tracking-tight">
+            No product found
+          </h3>
+          <UploadDrawerDialog />
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
-      <div className='flex justify-between'>
-        <h1 className='text-lg font-semibold md:text-2xl'>Inventory</h1>
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div className="flex justify-between">
+        <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
         {products && products.length > 0 && <UploadDrawerDialog />}
       </div>
 
       {products?.length === 0 || null ? (
-        <div className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm'>
-          <div className='flex flex-col items-center gap-1 text-center'>
+        <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+          <div className="flex flex-col items-center gap-1 text-center">
             <>
-              <h3 className='text-2xl font-bold tracking-tight'>
+              <h3 className="text-2xl font-bold tracking-tight">
                 You have no products
               </h3>
-              <p className='text-sm text-muted-foreground mb-5'>
+              <p className="text-sm text-muted-foreground mb-5">
                 You can start selling as soon as you add a product.
               </p>
               <UploadDrawerDialog />
@@ -32,7 +50,7 @@ export default function SellerProd() {
         <MasterTable data={products} seller={true} />
       ) : (
         isLoading && (
-          <div className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm'>
+          <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
             <Loader />
           </div>
         )
