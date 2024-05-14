61305cf0b0ca5f09ebb4c89eca1d5acd8f0831ae
GeroWalther <gero.walther@gmail.com>
2024-05-14 15:58:30 +0200

    stripeId must be updatable

--- src/trpc/api/controller/payment.ts
+++ src/trpc/api/controller/payment.ts
@@ -51,7 +51,7 @@ export const createSessionController = async (
 
   // , 'paypal', 'wechat_pay', 'klarna'
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    payment_method_types: ['card', 'paypal'],
     line_items: lineItem,
     mode: 'payment',
 
