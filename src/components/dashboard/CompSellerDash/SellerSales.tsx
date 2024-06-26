8823ab8e9e652b617bcc51414522f244e4391178
GeroWalther <gero.walther@gmail.com>
2024-06-26 20:52:26 +0200

    fixed SellerSales stripe_account_Id and payout_status

--- src/components/dashboard/CompSellerDash/SellerSales.tsx
+++ src/components/dashboard/CompSellerDash/SellerSales.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 import {
   Table,
   TableBody,
   TableCell,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-import { trpc } from "@/trpc/client";
-import { TransferMoneyButton } from "../StripeInteraction";
-import AddStripeAccountButton from "../AddStripeToAccount";
-import UpdateStripeAccountButton from "../UpdateStripeAccount";
+} from '@/components/ui/table';
+import { trpc } from '@/trpc/client';
+import { TransferMoneyButton } from '../StripeInteraction';
+import AddStripeAccountButton from '../AddStripeToAccount';
+import UpdateStripeAccountButton from '../UpdateStripeAccount';
 
 type SingleTransaction = {
   date: string;
@@ -23,13 +23,13 @@ type HistoryComponentProps = {
 };
 
 const HistoryComponent = ({ transactions, balance }: HistoryComponentProps) => {
-  const tableHeaders = ["Date", "Transaction", "Amount"];
+  const tableHeaders = ['Date', 'Transaction', 'Amount'];
   return (
-    <Table className="text-right">
+    <Table className='text-right'>
       <TableHeader>
         <TableRow>
           {tableHeaders.map((header, index) => (
-            <TableCell key={index} className="text-right font-bold">
+            <TableCell key={index} className='text-right font-bold'>
               {header}
             </TableCell>
           ))}
@@ -43,7 +43,7 @@ const HistoryComponent = ({ transactions, balance }: HistoryComponentProps) => {
             <TableCell>{transaction.amount}</TableCell>
           </TableRow>
         ))}
-        <TableRow className="font-bold">
+        <TableRow className='font-bold'>
           <TableCell></TableCell>
           <TableCell>Total</TableCell>
           <TableCell>{balance}</TableCell>
@@ -75,28 +75,28 @@ export default function SellerSales() {
     return <div>Error</div>;
   }
 
-  const { stripe_account_id, payout_status } = userStripeInfo;
+  const userStripeAccountId = userStripeInfo?.stripe_account_Id;
+  const userStripePayoutStatut = userStripeInfo?.payout_status;
 
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <h2 className="text-lg font-semibold md:text-2xl">Sales</h2>
+    <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+      <h2 className='text-lg font-semibold md:text-2xl'>Sales</h2>
       <div
-        className="bg-stone-100 p-4 py-8 rounded-lg shadow-md bg-cover"
+        className='bg-stone-100 p-4 py-8 rounded-lg shadow-md bg-cover'
         style={{
           backgroundImage:
-            "linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.8)), url(eis.jpg)",
-        }}
-      >
-        <h3 className="mb-4 text-md font-semibold text-stone-200">
+            'linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.8)), url(eis.jpg)',
+        }}>
+        <h3 className='mb-4 text-md font-semibold text-stone-200'>
           Ready for transfer
         </h3>
-        <h4 className="text-2xl font-bold mb-8 text-stone-200">$1000</h4>
-        <div className="flex justify-between">
+        <h4 className='text-2xl font-bold mb-8 text-stone-200'>$1000</h4>
+        <div className='flex justify-between'>
           <TransferMoneyButton balance={transActions?.balance as number} />
-          {payout_status && stripe_account_id ? null : (
+          {userStripePayoutStatut && userStripeAccountId ? null : (
             <AddStripeAccountButton />
           )}
-          {!payout_status && stripe_account_id ? (
+          {!userStripePayoutStatut && userStripeAccountId ? (
             <UpdateStripeAccountButton />
           ) : null}
         </div>
@@ -107,8 +107,8 @@ export default function SellerSales() {
           balance={transActions.balance}
         />
       )}
-      <div className="mt-6">
-        <h3 className="mb-4 text-xl font-semibold">Total Sales</h3>
+      <div className='mt-6'>
+        <h3 className='mb-4 text-xl font-semibold'>Total Sales</h3>
       </div>
     </main>
   );
